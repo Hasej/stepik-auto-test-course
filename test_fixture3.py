8ddcc0a0f0a0ba50be242f9a890dcb7e551addbe
novyjpolzovatel <1234>
2021-06-04 14:13:21 +0300

    pytest

--- test_fixture3.py
+++ test_fixture3.py
@@ -1,25 +1,26 @@
 import pytest
-from selenium import webdriver
-
-link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    # этот код выполнится после завершения теста
-    print("\nquit browser..")
-    browser.quit()
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
 
 
-class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
-    def test_guest_should_see_login_link(self, browser):
-        browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
 
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        # какие-то проверки
+
+    def test_second_smiling_faces(self, prepare_faces):
+        # какие-то проверки
