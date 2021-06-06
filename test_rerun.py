114768554f50b16e67c62822d419ed9bf4681755
novyjpolzovatel <1234>
2021-06-06 23:51:40 +0300

    pytest

--- test_rerun.py
+++ test_rerun.py
@@ -1,3 +1,15 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="class")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link_pass(browser):
