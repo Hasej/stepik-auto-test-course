c99d4dc15dd197d9e23c61ba3847a7d7aa93ea86
novyjpolzovatel <1234>
2021-06-06 23:38:52 +0300

    pytest

--- test_lesson3-6_step3.py
+++ test_lesson3-6_step3.py
@@ -5,46 +5,61 @@ from selenium import webdriver
 import time
 import math
 import pytest
+import unittest
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="class")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
+    time.sleep(5)
     browser.quit()
 
-lessons = [
-    ("236895"),
-    ("236896"),
-    ("236897"),
-    ("236898"),
-    ("236899"),
-    ("236903"),
-    ("236904"),
-    ("236905")
-]
+class TestAnswer():
+    lessons = [
+        ("236895"),
+        ("236896"),
+        ("236897"),
+        ("236898"),
+        ("236899"),
+        ("236903"),
+        ("236904"),
+        ("236905")
+    ]
+
     @pytest.mark.parametrize("lesson", lessons)
-    def test_get_correct_answer (browser, lesson):
-        link = f"https://stepik.org/lesson/{}/step/1".format(code)
+    def test_get_correct_answer(self, browser, lesson, result):
+        link = "https://stepik.org/lesson/{}/step/1".format(lesson)
         browser.get(link)
 
 
-        answer = math.log(int(time.time()))
+        answer = str(math.log(int(time.time())))
 
         input1 = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.TAG_NAME, 'textarea'))
         )
         input1.send_keys(answer)
 
-        button1 = browser.find_element_by_css_selector(.sumbit-submission)
+        button1 = browser.find_element_by_css_selector(".submit-submission")
         button1.click()
 
-        message = WebDriverWait(browser, 5).until(
-        EC.text_to_be_present_in_element(By.CSS_SELECTOR, '.smart-hints__hint')
-        )
+        time.sleep(3)
 
-        assert "Correct!" in message.text , "should be \"Correct!\" instead {message.text}"
+        message = browser.find_element_by_css_selector(".smart-hints__hint")
+
+        if message.text != "Correct!":
+            self.result += message.text
+            print(self.lessons)
+
+        assert message.text == "Correct!", f"should be \"Correct!\" instead \"{message.text}\""
+
+if __name__ == "__main__":
+    test_get_correct_answer()
+    print(self.result)
+
+
+
 
 
 
